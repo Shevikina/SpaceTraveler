0b5926cb769a511237c366d9e5954762725e76a1
Shevikina <Shevik.vlog@gmail.com>
2024-11-28 14:01:51 +0300

    Update color

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Color.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Color.kt
@@ -2,12 +2,7 @@ package com.stargalaxy.spacetraveler.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//val Purple80 = Color(0xFFD0BCFF)
-//val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-//val Purple40 = Color(0xFF6650a4)
-//val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val PrimaryColor = Color(0xFFFFBD13)
@@ -18,4 +13,3 @@ val SurfaceColor = Color(0xFFFFFFFF)
 val OnSurfaceColor = Color(0xFFA4A8B3)
 val BackgroundColor = Color(0xFFEFFAFD)
 val OnBackgroundColor = Color(0xFF071738)
-val back_logo = Color(0xFFFFFFFF)
