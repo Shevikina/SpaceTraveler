51fb2876fb420f8019366c7d49047ff15703be52
Shevikina <Shevik.vlog@gmail.com>
2025-04-24 19:55:31 +0300

    Fix gradle

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/screens/tours/views/TourCard.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/screens/tours/views/TourCard.kt
@@ -29,7 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import com.stargalaxy.spacetraveler.R
 import com.stargalaxy.spacetraveler.ui.theme.JetSpaceTravelerTheme
 import com.stargalaxy.spacetraveler.ui.theme.SpaceTravelerTheme
