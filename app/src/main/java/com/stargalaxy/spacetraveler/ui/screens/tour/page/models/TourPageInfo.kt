88f6587fa7e1798f970c3d82a81b76c26994a65c
Shevikina <Shevik.vlog@gmail.com>
2025-04-24 23:10:19 +0300

    Merge TourPageInfo with TourInfo

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/screens/tour/page/models/TourPageInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.stargalaxy.spacetraveler.ui.screens.tour.page.models
-
-import com.stargalaxy.spacetraveler.ui.screens.tours.models.TourInfo
-
-data class TourPageInfo(
-    val coreInfo: TourInfo,
-    val pageDescription: String,
-    val tourReviews: List<String>
-)
--- app/src/main/java/com/stargalaxy/spacetraveler/ui/screens/tours/models/TourInfo.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/screens/tours/models/TourInfo.kt
@@ -1,10 +1,15 @@
 package com.stargalaxy.spacetraveler.ui.screens.tours.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class TourInfo(
     val name: String,
-    val description: String,
-    val imagePath: String,
+    val type: String,
+    val image: String,
     val rating: Double,
-    val reviews: Int,
+    val pageDescription: String,
+    val reviewsCount: Int,
+    val tourReviews: List<String> = emptyList(),
     val isNew: Boolean = false,
 )
